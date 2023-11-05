2499a0f866fe9462c453e481fb8e907d05805b59
shusuke919 <sh091965@gmail.com>
2023-11-05 11:48:31 +0900

    developブランチ作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ export default function Home() {
   return (
     <main className={styles.main}>
       <div className={styles.description}>
+        <div>mukuJiro</div>
         <p>
           Get started by editing&nbsp;
           <code className={styles.code}>src/app/page.tsx</code>
